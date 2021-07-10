9f465031b6d0529b661223b1890be59f79072e26
hieu1966hn <hieu1966hn@gmail.com>
2021-07-10 21:26:48 +0700

    new done query

--- SS11/app.js
+++ SS11/app.js
@@ -1,3 +1,27 @@
-db.collection("cafes").get().then((snapshot)=>{
-    console.log(snapshot.docs);
+const cafeList = document.getElementById('cafe-list');
+
+function renderCafe(doc) {
+    let li = document.createElement("li");
+    let name = document.createElement('span');
+    let city = document.createElement('span');
+
+    li.setAttribute('data-id', doc.id);
+    // luu lai id cua  doc do trong li
+
+    name.textContent = doc.data().name;
+    city.textContent = doc.data().city;
+
+    li.appendChild(name);
+    li.appendChild(city);
+
+
+    cafeList.appendChild(li)
+}
+
+
+db.collection("cafes").get().then((snapshot) => {
+    snapshot.docs.forEach(doc => {
+        console.log(doc.data());
+        renderCafe(doc);
+    })
 });
